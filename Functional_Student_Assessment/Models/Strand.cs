451ed1b5f6e56ff7228f70ba5dac3cd9b7d02ccc
Arandia-AericaMae-BSIT32E1 <arandiaaerica@gmail.com>
2024-06-14 02:40:20 +0800

    added new clss in Models named Strand.cs

---
+++ Functional_Student_Assessment/Models/Strand.cs
@@ -0,0 +1,9 @@
+﻿namespace Functional_Student_Assessment.Models
+{
+    public class Strand
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        // Define criteria or rules for each strand if needed
+    }
+}
