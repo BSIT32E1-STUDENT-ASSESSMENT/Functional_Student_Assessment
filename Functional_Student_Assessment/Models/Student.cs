76356343190992e35ff0ceed8f62b194016e8161
Tagumpay-Cristian-BSIT32E1 <161043308+Tagumpay-Cristian-BSIT32E1@users.noreply.github.com>
2024-06-14 02:56:02 +0800

    Added Code to Student.cs

---
+++ Functional_Student_Assessment/Models/Student.cs
@@ -0,0 +1,6 @@
+﻿public class Student
+{
+    public string Name { get; set; }
+    public Dictionary<string, Dictionary<string, double>> Grades { get; set; } // Dictionary<GradeLevel, Dictionary<Subject, Grade>>
+    public string Strand { get; set; }
+}
