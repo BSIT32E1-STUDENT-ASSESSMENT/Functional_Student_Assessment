09885699c9d6c2450c8539aebe1524cffd4d759c
Arandia-AericaMae-BSIT32E1 <arandiaaerica@gmail.com>
2024-06-14 02:44:23 +0800

    updates the code of program.cs

--- Functional_Student_Assessment/Data/ApplicationDbContext.cs
+++ Functional_Student_Assessment/Data/ApplicationDbContext.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Functional_Student_Assessment.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Functional_Student_Assessment.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Strand> Strands { get; set; }
     }
 }
