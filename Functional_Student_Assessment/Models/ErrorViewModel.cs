a96ab4018ad9f5a002ad7341a3ee3f34d8f30da8
AndoJoshuaBSIT32E1 <160818162+AndoJoshuaBSIT32E1@users.noreply.github.com>
2024-06-14 02:29:39 +0800

    Add project files.

---
+++ Functional_Student_Assessment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Functional_Student_Assessment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
