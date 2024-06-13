a510f38d33763e1e0e34de970535081b9cc1ce98
Arandia-AericaMae-BSIT32E1 <arandiaaerica@gmail.com>
2024-06-14 03:15:42 +0800

    added new views in Student folder  named index.cshtml

--- Functional_Student_Assessment/Controllers/StudentController.cs
+++ Functional_Student_Assessment/Controllers/StudentController.cs
@@ -1,60 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Functional_Student_Assessment.Data;
+using System.Web.Mvc;
 using Functional_Student_Assessment.Models;
-using System.Linq;
-using System.Collections.Generic;
 
-namespace Functional_Student_Assessment.Controllers
+public class StudentController : Controller
 {
-    public class StudentController : Controller
+    public ActionResult Index()
     {
-        private readonly ApplicationDbContext _context;
+        return View(InMemoryDatabase.StudentGrades);
+    }
 
-        public StudentController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+    public ActionResult AddGrade()
+    {
+        return View();
+    }
 
-        // Action for teachers to enter grades
-        [HttpGet]
-        public IActionResult EnterGrades()
-        {
-            return View();
-        }
+    [HttpPost]
+    public ActionResult AddGrade(StudentGrade studentGrade)
+    {
+        InMemoryDatabase.StudentGrades.Add(studentGrade);
+        return RedirectToAction("Index");
+    }
 
-        [HttpPost]
-        public IActionResult EnterGrades(int studentId, List<Grade> grades)
+    public ActionResult RecommendStrand(int id)
+    {
+        var studentGrade = InMemoryDatabase.StudentGrades.FirstOrDefault(s => s.Id == id);
+        if (studentGrade == null)
         {
-            var student = _context.Students.Find(studentId);
-            if (student != null)
-            {
-                student.Grades.AddRange(grades);
-                _context.SaveChanges();
-            }
-            return RedirectToAction("Index", "Home");
+            return HttpNotFound();
         }
-
-        // Action for students to view their strand
-        public IActionResult ViewStrand(int studentId)
-        {
-            var student = _context.Students.Find(studentId);
-            if (student != null)
-            {
-                student.Strand = DetermineStrand(student);
-                _context.SaveChanges();
-                return View(student);
-            }
-            return NotFound();
-        }
-
-        private string DetermineStrand(Student student)
-        {
-            // Implement your logic to determine the strand based on grades
-            // Example logic (simplified):
-            double average = student.Grades.Average(g => g.Score);
-            if (average >= 90) return "STEM";
-            if (average >= 75) return "ABM";
-            return "HUMSS";
-        }
+        var recommendedStrand = InMemoryDatabase.GetRecommendedStrand(studentGrade);
+        ViewBag.RecommendedStrand = recommendedStrand;
+        return View(studentGrade);
     }
 }
+
