95a518abeeaec6ef85ce8ce0642eb1f94c580d42
Arandia-AericaMae-BSIT32E1 <arandiaaerica@gmail.com>
2024-06-14 02:38:47 +0800

    added new class in Models named Grade.cs

---
+++ Functional_Student_Assessment/Models/Grade.cs
@@ -0,0 +1,13 @@
+﻿namespace Functional_Student_Assessment.Models
+{
+    public class Grade
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+        public string Subject { get; set; }
+        public int Year { get; set; }
+        public double Score { get; set; }
+    }
+
+}
