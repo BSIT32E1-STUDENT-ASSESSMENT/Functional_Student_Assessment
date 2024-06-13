1c846ad1e574fce3a5ce3cad87e020330ef56485
Arandia-AericaMae-BSIT32E1 <arandiaaerica@gmail.com>
2024-06-14 03:08:57 +0800

    added new controller named StudentGrade.cs

---
+++ Functional_Student_Assessment/Models/StudentGrade.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Functional_Student_Assessment.Models
+{
+    public class StudentGrade
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public int GradeLevel { get; set; }
+        public double Math { get; set; }
+        public double English { get; set; }
+        public double Science { get; set; }
+        // Add other subjects as needed
+    }
+
+}
