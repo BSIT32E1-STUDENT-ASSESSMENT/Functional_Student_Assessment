d96bd43e4e5502e3c0ed71cbd448fff1ab040390
Tagumpay-Cristian-BSIT32E1 <161043308+Tagumpay-Cristian-BSIT32E1@users.noreply.github.com>
2024-06-14 21:03:33 +0800

    Changed the add grade to dropdown

--- Functional_Student_Assessment/Models/StudentGrade.cs
+++ Functional_Student_Assessment/Models/StudentGrade.cs
@@ -5,7 +5,7 @@ namespace Functional_Student_Assessment.Models
     {
         public int Id { get; set; }
         public string StudentName { get; set; }
-        public int GradeLevel { get; set; }
+        public string GradeLevel { get; set; }
         public double Math { get; set; }
         public double English { get; set; }
         public double Science { get; set; }
