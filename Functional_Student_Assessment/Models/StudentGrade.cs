0a3facf78eece6091c273cd3e95c0eee5746c2c6
Arandia-AericaMae-BSIT32E1 <arandiaaerica@gmail.com>
2024-06-14 20:55:37 +0800

    updates the StudentGrade.cs

--- Functional_Student_Assessment/Models/StudentGrade.cs
+++ Functional_Student_Assessment/Models/StudentGrade.cs
@@ -1,26 +1,22 @@
 ﻿
 namespace Functional_Student_Assessment.Models
 {
-   public class StudentGrade
-{
-    public int Id { get; set; }
+    public class StudentGrade
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public int GradeLevel { get; set; }
+        public int Math { get; set; }
+        public int English { get; set; }
+        public int Science { get; set; }
+        public int Filipino { get; set; }
+        public int History { get; set; }
 
-    
-    public string StudentName { get; set; }
-
-    
-    public int GradeLevel { get; set; }
-
-    public int Math { get; set; }
-
-    public int English { get; set; }
-
-    
-    public int Science { get; set; }
-
-    // New properties for recommended strand
-    public string RecommendedStrandName { get; set; }
-    public string RecommendedStrandDescription { get; set; }
-}
+        // Properties for recommendations
+        public string RecommendedStrandName1 { get; set; }
+        public string RecommendedStrandDescription1 { get; set; }
+        public string RecommendedStrandName2 { get; set; }
+        public string RecommendedStrandDescription2 { get; set; }
+    }
 
 }
