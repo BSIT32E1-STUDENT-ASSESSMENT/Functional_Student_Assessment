36056e05c004ff3d171d99eab07660c3ccbb8181
Tagumpay-Cristian-BSIT32E1 <161043308+Tagumpay-Cristian-BSIT32E1@users.noreply.github.com>
2024-06-14 22:25:38 +0800

    Same student number cannot be added

--- Functional_Student_Assessment/Controllers/StudentController.cs
+++ Functional_Student_Assessment/Controllers/StudentController.cs
@@ -18,9 +18,18 @@ public class StudentController : Controller
     [HttpPost]
     public ActionResult AddGrade(StudentGrade studentGrade)
     {
+        // Check if the student number already exists
+        if (InMemoryDatabase.StudentGrades.Any(s => s.StudentNumber == studentGrade.StudentNumber))
+        {
+            ModelState.AddModelError("StudentNumber", "Student number already exists.");
+            return View(studentGrade);
+        }
+
+        // If the student number is unique, add the grade
         InMemoryDatabase.StudentGrades.Add(studentGrade);
         return RedirectToAction("Index");
     }
+
 
     public ActionResult RecommendStrand(int id)
     {
